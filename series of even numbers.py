e578120afca07033a2d7e8fb928bd38ca77539c5
himanid27 <138992332+HimaniD27@users.noreply.github.com>
2024-07-01 23:37:41 +0530

    sorting

---
+++ series of even numbers.py
@@ -0,0 +1,13 @@
+def evenNumberSeries(n):     #for first n numbers
+  series=[]
+  for i in range(n):
+    series.append(2*i)
+  return series
+
+print(evenNumberSeries(10))
+
+
+
+
+for n in range(5):     #for first 5 even numbers
+    print(2*n)
