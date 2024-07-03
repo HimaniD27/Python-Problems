09fb4537f8ee6c73db470d66d1681e3937600601
himanid27 <138992332+HimaniD27@users.noreply.github.com>
2024-07-03 23:59:28 +0530

    solution

---
+++ replacing a substring in a string.py
@@ -0,0 +1,4 @@
+def replaceSubstring(original_string,old_substring,new_substring):
+  return original_string.replace(old_substring,new_substring)
+
+print(replaceSubstring("let's play.","play","study"))
